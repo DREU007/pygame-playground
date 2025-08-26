ca33f9a6ffccde190eb2a7260f9862664874cd78
Denis Bolshakov <dreu007@gmail.com>
2025-08-26 07:18:35 +0100

    Feat: coin collection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+
+from random import randrange
 import pygame as pg
 from settings import Resolution
+
+
+COIN_SIZE = 20
 
 class Game:
     def __init__(self):
@@ -8,15 +13,24 @@ class Game:
         self.clock = pg.time.Clock()
         self.running = True
 
-
+        self.score = 0
         self.dt = 0
         self.player = pg.Vector2(
             self.screen.get_width() / 2,
             self.screen.get_height() / 2
         )
 
+        self.set_random_coin()
+
+    def set_random_coin(self):
+
+        rw = randrange(0, self.screen.get_width() - COIN_SIZE)
+        rh = randrange(0, self.screen.get_height() - COIN_SIZE) 
+        self.coin = pg.Rect(rw, rh, COIN_SIZE, COIN_SIZE)
+
     def render(self):
-        pg.draw.circle(self.screen, "red", self.player, 40)
+        pg.draw.rect(self.screen, "yellow", self.coin)
+        pg.draw.circle(self.screen, "red", self.player, 10)
 
         keys=pg.key.get_pressed()
         if keys[pg.K_w]: self.player.y -= 300 * self.dt
@@ -24,6 +38,10 @@ class Game:
         if keys[pg.K_a]: self.player.x -= 300 * self.dt
         if keys[pg.K_d]: self.player.x += 300 * self.dt
 
+        is_collision = self.coin.collidepoint(self.player)
+        if is_collision:
+            self.score += 1
+            self.set_random_coin()
 
     def main(self):
 
@@ -41,5 +59,5 @@ class Game:
         pg.quit()
 
 if __name__ == "__main__":
-    g = Game()
-    g.main()
+   g = Game()
+   g.main()
